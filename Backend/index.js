226ea35c640e1ce88cb0551739d0a5679be21835
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2025-01-17 00:34:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const port = process.env.PORT || 3000;
+const port =3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-module.exports=app
