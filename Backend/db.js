7e49e21bf1c3d146a3a6beb486a90e501076462c
unknown <riteshraba251104@gmail.com>
2025-01-16 21:51:17 +0530

    add db.js

---
+++ Backend/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+let isConnected; // Track the connection state
+
+async function connectToDatabase() {
+  if (isConnected) {
+    console.log('Reusing existing MongoDB connection');
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect('mongodb+srv://riteshrana251104:Wme7KRrm6nY2u1D8@airline.j35ch.mongodb.net/airline?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+}
+
+module.exports = connectToDatabase;
